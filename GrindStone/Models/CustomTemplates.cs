1ed11910fb0d96bffd5ad53c911eac8466acf882
Jared Merten <jarmerten@gmail.com>
2016-06-01 14:20:30 -0500

    Custom Template model built

---
+++ GrindStone/Models/CustomTemplates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class CustomTemplates
+    {
+        [Key]
+        
+        public int TemplateId { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+        [Required, StringLength(50), Display(Name = "Title")]
+        public string TemplateName { get; set; }
+        public int Location { get; set; }
+    }
+}
