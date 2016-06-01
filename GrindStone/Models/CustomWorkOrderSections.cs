22b015cd845d34818ecfc5013a2b0f2a60c24c38
Jared Merten <jarmerten@gmail.com>
2016-06-01 12:22:28 -0500

    Added custom work order sections model

---
+++ GrindStone/Models/CustomWorkOrderSections.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class CustomWorkOrderSections
+    {
+        [Key]
+        public int SectionId { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+        [Required, StringLength(50), Display(Name = "Section Title")]
+        public string Title { get; set; }
+
+
+
+    }
+}
