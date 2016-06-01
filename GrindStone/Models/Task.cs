553631c883316ac3c6ad1be6e573aca03bbd426a
Jared Merten <jarmerten@gmail.com>
2016-06-01 13:53:34 -0500

    built model for tasks

---
+++ GrindStone/Models/Task.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class Task
+    {
+        [Key]
+
+        public int TaskId { get; set; }
+
+        [ForeignKey("Jobs")]
+        public int JobId { get; set; }
+        public virtual Jobs Jobs { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+        [Required]
+        [Display(Name = "Task Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Start Day")]
+        public int Date{ get; set; }
+
+        [Required]
+        [Display(Name = "Days Long")]
+        public int TaskLength { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
