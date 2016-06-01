c4d635e1864e433cfa7ee41646ae741e49f53404
Jared Merten <jarmerten@gmail.com>
2016-06-01 14:15:55 -0500

    Build products model

---
+++ GrindStone/Models/Products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+        [Required, StringLength(50), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(1000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        [Display(Name = "Sku")]
+        public int Sku { get; set; }
+    }
+}
