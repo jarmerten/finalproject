d58fdd8324555c3b989aecbeebc463e081b4fad2
Jared Merten <jarmerten@gmail.com>
2016-06-01 14:00:15 -0500

    Built up model for completing tasks

---
+++ GrindStone/Models/FinishedTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class FinishedTask
+    {
+        [Key]
+        public int FinishedTaskId { get; set; }
+
+        [ForeignKey("Task")]
+        public int TaskId { get; set; }
+        public virtual Task Task { get; set; }
+
+        [Display(Name = "Comments")]
+        public string Comments { get; set; }
+
+        [Required]
+        [Display(Name = "Date Finished")]
+        public int FinishDate { get; set; }
+
+        public bool approval { get; set; }
+
+    }
+}
