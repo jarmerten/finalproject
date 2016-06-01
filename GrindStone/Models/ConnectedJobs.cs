fd05205b3be939018c28b4705865f7184c920c7a
Jared Merten <jarmerten@gmail.com>
2016-06-01 14:03:08 -0500

    build connected jobs model

---
+++ GrindStone/Models/ConnectedJobs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class ConnectedJobs
+    {
+        [Key]
+        public int ConnectedJobsId { get; set; }
+
+        [ForeignKey("Jobs")]
+        public int JobId { get; set; }
+        public virtual Jobs Jobs { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+    }
+}
