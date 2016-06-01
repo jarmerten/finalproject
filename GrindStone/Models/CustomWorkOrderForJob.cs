91cc1847a7eed9c0e96117989ea9c0f529bddfba
Jared Merten <jarmerten@gmail.com>
2016-06-01 12:25:31 -0500

    Added Custom Work Order model for specific jobs

---
+++ GrindStone/Models/CustomWorkOrderForJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class CustomWorkOrderForJob
+    {
+        [Key]
+        public int JobWorkOrderId { get; set; }
+
+        [ForeignKey("Jobs")]
+        public int JobId { get; set; }
+        public virtual Jobs Jobs { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+    }
+}
