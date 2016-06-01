ca848790b108c24fe66baae651eb43ae90b46bab
Jared Merten <jarmerten@gmail.com>
2016-06-01 09:00:35 -0500

    Add Project

---
+++ GrindStone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrindStone.Startup))]
+namespace GrindStone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
