0ce07cac6beb1b71f6811df0f2af4357017c037b
Jared Merten <jarmerten@gmail.com>
2016-06-01 14:07:31 -0500

    Added model for alerts

---
+++ GrindStone/Models/Alert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GrindStone.Models
+{
+    public class Alert
+    {
+        [Key]
+        public int AlertId { get; set; }
+
+        [ForeignKey("Jobs")]
+        public int JobId { get; set; }
+        public virtual Jobs Jobs { get; set; }
+
+        [ForeignKey("RegisterViewModel")]
+        public int BusinessId { get; set; }
+        public virtual RegisterViewModel RegisterViewModel { get; set; }
+
+        [ForeignKey("Task")]
+        public int TaskId { get; set; }
+        public virtual Task Task { get; set; }
+
+        [Required]
+        [Display(Name = "Alert Type")]
+        public string AlertType { get; set; }
+    }
+}
