b435d800434bbd13569478c67e9e3c0cc6e8dcc0
Jared Merten <jarmerten@gmail.com>
2016-06-03 09:36:07 -0500

    Updated Work order model

--- GrindStone/Models/CustomWorkOrderForJob.cs
+++ GrindStone/Models/CustomWorkOrderForJob.cs
@@ -16,10 +16,67 @@ namespace GrindStone.Models
         public int JobId { get; set; }
         public virtual Jobs Jobs { get; set; }
 
+        [Required, StringLength(100), Display(Name = "Title")]
         public string Title { get; set; }
 
-        public string Desciption { get; set; }
+        [Required, StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle { get; set; }
 
-        public int LocationOnPage { get; set; }
+        [Required, StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle1 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description1 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle2 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description2 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle3 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description3 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle4 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description4 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle5 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description5 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle6 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description6 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle7 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description7 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle8 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description8 { get; set; }
+
+        [StringLength(100), Display(Name = "Section Title")]
+        public string SectionTitle9 { get; set; }
+
+        [StringLength(10000), Display(Name = "Description"), DataType(DataType.MultilineText)]
+        public string Description9 { get; set; }
     }
 }
